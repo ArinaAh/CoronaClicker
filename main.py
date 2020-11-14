62a219efcf75e65aa3a38bc2c37fc0e3e3de2d42
Arina <ahmetsafina.af@phystech.edu>
2020-11-14 22:27:52 +0300

    Изменения: 1.Решена проблема с зажатием клавиши пробел и "накручивания очков". Теперь, чтобы понизить уровень заражения, нужно нажимать не пробел, а кнопку в главном окне "Убить вирус". 2.Длина строк соответствует PEP-8. 3.Убран import*, используется: import tkinter as tk. 4.Использованы константы для чисел. 5.Отрегулирована "сложность" игры. Скорость изменения уровня заражения увеличивается с увеличением дня заражения, после того как куплен первый автокликер.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from tkinter import *
+import tkinter as tk
 import random
 
 virus = 0
+virus_increaser = 1
 amount_of_viruses = 0
 viruses = []
 day = 0
@@ -9,6 +10,12 @@ Enter = True
 money = 0
 count = 0
 autoclickers = []
+time_change_screen = 100
+time_auto_handler = 500
+time_to_update_virus = 150
+stop_number = 1000
+time_to_plus_day = 11000
+plus_money = 0.5
 
 
 class AutoClicker:
@@ -37,10 +44,11 @@ def createAutoClicker(value, point):
 
 def autoClickersHandler():
     global virus
+    global time_auto_handler
     for i in autoclickers:
         if virus > i.get_point():
             virus -= i.get_point()
-    inf_virus.after(500, autoClickersHandler)
+    inf_virus.after(time_auto_handler, autoClickersHandler)
 
 
 def play(event):
@@ -51,47 +59,53 @@ def play(event):
         main_text.config(text="")
         updateVirus()
         changeDay()
-        changeScrin()
+        changeScreen()
         autoClickersHandler()
         Enter = False
 
 
-def changeScrin():
+def changeScreen():
     global virus
     global day
     global money
     inf_virus.config(text="Уровень распространения:" + str(virus))
     inf_day.config(text="День:" + str(day))
     inf_money.config(text='Деньги:' + str(money))
-    virus_scrin.after(100, changeScrin)
+    virus_screen.after(time_change_screen, changeScreen)
 
 
 def updateVirus():
     global viruses
     global virus
     global amount_of_viruses
-    virus += 1
+    global virus_increaser
+    global time_to_update_virus
+    virus += virus_increaser
     if amount_of_viruses < virus // 10:
         amount_of_viruses += 1
-        Label(root, image=photo_virus).grid(row=random.randint(3, 10), column=random.randint(3, 10))
-        Label(root, image=photo_virus).grid(row=random.randint(3, 10), column=random.randint(3, 10))
-        Label(root, image=photo_virus).grid(row=random.randint(3, 10), column=random.randint(3, 10))
+        for i in range(2):
+            tk.Label(root, image=photo_virus).grid(row=random.randint(3, 10),
+                                                   column=random.randint(3, 10))
+
     if amount_of_viruses > virus // 10:
         amount_of_viruses -= 1
         grid_slaves = root.grid_slaves()
         for l in grid_slaves:
             l.destroy()
-    if notKorona():
+    if notQuarantine():
         if len(autoclickers) > 0:
-            inf_virus.after(150 - len(autoclickers) * 3, updateVirus)
-        else:
-            inf_virus.after(150, updateVirus)
+            virus_increaser = 1 + day / 2
+        inf_virus.after(time_to_update_virus, updateVirus)
 
 
-def notKorona():
+def notQuarantine():
     global virus
-    if virus >= 1000:
-        finalLabel = Label(root, image=photo_final_virus).grid(row=0, column=0)
+    global stop_number
+    if virus >= stop_number:
+        finalLabel1 = tk.Label(root, text="Прожито дней: " + str(day),
+                               font=("Helvetica", 25)).grid(row=1, column=0)
+        finalLabel = tk.Label(root, image=photo_final_virus).grid(row=2, column=0)
+        title.destroy()
         return False
     else:
         return True
@@ -99,32 +113,44 @@ def notKorona():
 
 def changeDay():
     global day
-    day += 1
-    if notKorona():
-        inf_day.after(5000, changeDay)
+    global time_to_plus_day
+
+    if notQuarantine():
+        day += 1
+        inf_day.after(time_to_plus_day, changeDay)
 
 
-def stopKorona(event):
+def stopKorona():
     global money
     global virus
-    if notKorona():
+    global plus_money
+    if notQuarantine():
         if virus > 0:
             virus -= 1
-            money += 0.5
+            money += plus_money
 
 
-root = Tk()
+root = tk.Tk()
 root.title("Дави их!#мирбезкороны")
 root.geometry("500x500")
 
-title = Label(root, text="Давай раздавим их!", font=("Helvetica", 12))
-main_text = Label(root, text="Нажми Enter чтобы начать", font=("Helvetica", 10))
-inf_virus = Label(root, text="Уровень распространения:" + str(virus), font=("Helvetica", 12))
-inf_day = Label(root, text="День:" + str(day), font=("Helvetica", 12))
-inf_money = Label(root, text='Деньги:' + str(money), font=("Helvetica", 12))
-mask_buy = Button(root, text='Купи маску 10', command=lambda: createAutoClicker(10, 1))
-antiseptic_buy = Button(root, text='Купи антисептик 50', command=lambda: createAutoClicker(50, 5))
-vaccine_buy = Button(root, text='Купи вакцину 100', command=lambda: createAutoClicker(100, 10))
+title = tk.Label(root, text="Давай раздавим их!", font=("Helvetica", 12))
+main_text = tk.Label(root, text="Нажми Enter чтобы начать",
+                     font=("Helvetica", 10))
+inf_virus = tk.Label(root, text="Уровень распространения:" + str(virus),
+                     font=("Helvetica", 12))
+inf_day = tk.Label(root, text="День:" + str(day),
+                   font=("Helvetica", 12))
+inf_money = tk.Label(root, text='Деньги:' + str(money),
+                     font=("Helvetica", 12))
+mask_buy = tk.Button(root, text='Купи маску 10',
+                     command=lambda: createAutoClicker(10, 1))
+antiseptic_buy = tk.Button(root, text='Купи антисептик 50',
+                           command=lambda: createAutoClicker(50, 5))
+vaccine_buy = tk.Button(root, text='Купи вакцину 100',
+                        command=lambda: createAutoClicker(100, 10))
+button_minus_virus = tk.Button(root, text='Убить вирус', height=5,
+                               background="#C71585", width=10, command=lambda: stopKorona())
 
 title.place(x=180, y=10)
 main_text.place(x=180, y=40)
@@ -134,11 +160,10 @@ inf_money.place(x=0, y=100)
 mask_buy.place(x=10, y=320)
 antiseptic_buy.place(x=10, y=350)
 vaccine_buy.place(x=10, y=380)
+button_minus_virus.place(x=200, y=200)
 
-photo_final_virus = PhotoImage(file="virus4.png")
-photo_virus = PhotoImage(file="virus.png")
-virus_scrin = Label(root, image=photo_virus)
-
-root.bind('<space>', stopKorona)
+photo_final_virus = tk.PhotoImage(file="virus4.png")
+photo_virus = tk.PhotoImage(file="virus.png")
+virus_screen = tk.Label(root, image=photo_virus)
 root.bind('<Return>', play)
 root.mainloop()
